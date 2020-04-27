b7d4f18f3a005de2a5db2d6bec3dc22e059643df
Luis <loic.quiros@gmail.com>
2020-04-27 15:10:49 -0700

    sock-merchant phyton/ second lines

--- sock-merchant/sock-merchant.py
+++ sock-merchant/sock-merchant.py
@@ -1,9 +1,20 @@
 def sockMerchant(n, ar):
-    print(n)
-    print(ar)
+    i = j = result = 0
+    array = 101 * [0]
+
+    while i < n:
+        array[ar[i]] += 1
+        i += 1 
+    int(result)
+    while j < 101:
+        result += array[j] / 2
+        j += 1 
+    return result
 
 def main():
-    sockMerchant(6, 7)
+    n = 9
+    ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+    print(sockMerchant(n, ar))
 
 if __name__ == "__main__":
     main()
